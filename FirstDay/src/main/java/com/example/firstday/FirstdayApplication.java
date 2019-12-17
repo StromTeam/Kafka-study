d79eeac86d6e57171345b6be1346a17e4d4a3be7
baofeng <baofeng@jimilab.com>
2019-12-17 17:48:38 +0800

    demo

---
+++ FirstDay/src/main/java/com/example/firstday/FirstdayApplication.java
@@ -0,0 +1,13 @@
+package com.example.firstday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstdayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstdayApplication.class, args);
+    }
+
+}
